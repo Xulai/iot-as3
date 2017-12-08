d25451955fa5d519e64cc8a1699c66e3962c25f2
Dharius Robinson <dr296@kent.ac.uk>
2017-12-08 15:02:42 +0000

    Data smoothing - incomplete

--- src/Device/DeviceGraph.js
+++ src/Device/DeviceGraph.js
@@ -17,6 +17,7 @@ class DeviceGraph extends Component {
       samples: null,
       samples2: null,
       error: false,
+      acceptableVariance: null
     };
   }
 
@@ -30,6 +31,122 @@ class DeviceGraph extends Component {
     }
   }
 
+  //Returns acceptable percentage variance between two values based on sample rate
+  //Anything outside of this range will be treated as anomalous values
+  checkAcceptableVariance() {
+    const sampleRate = this.props.sampleRate;
+    if(sampleRate === "minute")
+      return 5; //+-5% acceptable variance
+    else if(sampleRate === "10minute")
+      return 20; //+-20% acceptable variance
+    else if(sampleRate === "hour")
+      return 100; //+-100% acceptable variance
+  }
+
+
+  isAnomalous(val1, val2)
+  {
+	  var var1Variance = (val1/100) * 10;//this.checkAcceptableVariance();
+	  if(Math.abs(val1 - val2) > var1Variance) {
+		 return true; 
+	  }
+	  return false;
+  }
+
+  checkAndFixAnomalousVals(values) {
+    var prePreviousValue;
+	  var middleValue;
+	  var currentValue;
+    for(var i = 2; i < values.length; i++) {
+      prePreviousValue = values[i-2][1];
+      middleValue = values[i-1][1];
+      currentValue = values[i][1];
+      if(this.isAnomalous(prePreviousValue, middleValue) && this.isAnomalous(currentValue, middleValue)) { //this means that the middle value is out of range of both surrounding values
+        console.log([values[i-1][1], (prePreviousValue + currentValue) / 2]);
+        console.log([prePreviousValue, middleValue, currentValue]);
+        values[i-1][1] = (prePreviousValue + currentValue) / 2; //set anomalous middle value to average of surrounding values
+        console.log(values[i-1][1]);
+      }
+    }
+    return values;
+  }
+
+  findEndPosOfTimespan(values, startTime, timespan)
+  {
+    for(var position = 0; position < values.length; position++)
+    {
+      if((values[position][0] - timespan - timespan) > 0)
+      {
+        if (position === values.length - 1)
+          return values.length - 2;
+        else
+          return position;
+      }
+      return position;
+    }
+    return null;
+  }
+
+  findPosition(values, startTime)
+  {
+    for(var position = 0; position < values.length; position++) 
+    {
+      if (values[position][0] > startTime)
+        if (position === 0)
+          return 1;
+        else
+          return position;
+    }
+    return null;
+  }
+
+  avgOfTimespan(values, timeSpan, startTime)
+  {
+    var total = 0;
+    var counter = this.findPosition(values, startTime);
+    while ((values[counter][0] - startTime - timeSpan) <= 0) {
+      if(counter >= values.length)
+      {
+        return total/counter;
+      }
+      total += values[counter][1];
+      counter++;
+    }
+    return total/counter;
+  }
+
+  smoothValues(values) {
+    var timespan;
+    var avgofTimespan;
+    var avg;
+    var startTime = values[0][1];
+    if(this.props.sampleRate = "minute")
+      timespan = 1000; // 1 second
+    else if(this.props.sampleRate = "10minute")
+      timespan = 10000; // 10 seconds
+    else if(this.props.sampleRate = "hour")
+      timespan = 60000; // 1 minute
+    
+    while(startTime <= values[values.length-1][0]){
+      avgofTimespan = this.avgOfTimespan(values, timespan, startTime);
+      for(var i = this.findPosition(values, startTime); i <= this.findEndPosTimespan(values, startTime, timespan); i++)
+      {
+        //if(isAnomalous(values[i-1][0], values[i][0]) && isAnomalous(values[i+1][0], values[i][0]))
+        avg = (values[i-1][0] + values[i+1][0]) / 2;
+        if(Math.abs(avgofTimespan - avg) > (avgofTimespan / 10))
+        {
+          if((avgofTimespan - avg) > 0)
+            values[i][0] = avg + (avgofTimespan/10);
+          else
+            values[i][0] = avg - (avgofTimespan/10);
+        }
+      }
+      startTime += timespan;
+    }
+
+    return values;
+  }
+
   getValues(sampleRate) {
     DeviceHelper.showSampleRate(this.props.device, sampleRate)
     .then(response => {
@@ -40,7 +157,9 @@ class DeviceGraph extends Component {
 
       if(!_.isEmpty(response.data.light_value)) {
         sensorName = "light";
-        values = response.data.light_value.map(value => [parseInt(moment(value[0]).format('X')), value[1]]);
+        values = this.smoothValues(response.data.light_value.map(value => parseInt[moment(value[0]).format('X'), value[1]]));
+        //values = response.data.light_value.map(value => [parseInt(moment(value[0]).format('X')), value[1]]);
+        //values = this.checkAndFixAnomalousVals(values);
         data = {
           "name": "Light values",
           "columns": ["time", "value"],
@@ -49,6 +168,7 @@ class DeviceGraph extends Component {
       } else if(!_.isEmpty(response.data.gas_values)) {
         sensorName = "gas";
         values = response.data.gas_values.map(value => [parseInt(moment(value[0]).format('X')), value[1]]);
+        values = this.checkAndFixAnomalousVals(values);
         data = {
         "name": "CO2 Generator",
         "columns": ["time", "value"],
@@ -57,6 +177,7 @@ class DeviceGraph extends Component {
       } else if(!_.isEmpty(response.data.solar_value)) {
         sensorName = "solar";
         values = response.data.solar_value.map(value => [parseInt(moment(value[0]).format('X')), value[1]]);
+        values = this.checkAndFixAnomalousVals(values);
         data = {
           "name": "Solar values",
           "columns": ["time", "value"],
@@ -65,6 +186,7 @@ class DeviceGraph extends Component {
       } else if(!_.isEmpty(response.data.moisture_value)) {
         sensorName = "hydrometer";
         values = response.data.moisture_value.map(value => [parseInt(moment(value[0]).format('X')), value[1]]);
+        values = this.checkAndFixAnomalousVals(values);
         data = {
           "name": "Soil moisture values",
           "columns": ["time", "value"],
@@ -73,14 +195,15 @@ class DeviceGraph extends Component {
       } else if(!_.isEmpty(response.data.temperature_value)) {
         sensorName = "Temperature and Humidity";
         values = response.data.temperature_value.map(value => [parseInt(moment(value[0]).format('X')), value[1]]);
+        values = this.checkAndFixAnomalousVals(values);
         data = {
           "name": "Temperature",
           "columns": ["time", "value"],
           "points": values
         };
         
-        //Don't think this is actually doing anything here - Trying to create both temp graphs and humidity graphs on the same page
         var humidityValues = response.data.humidity_value.map(value => [parseInt(moment(value[0]).format('X')), value[1]]);
+        humidityValues = this.checkAndFixAnomalousVals(humidityValues)
         var humidityData = {
           "name": "Humidity",
           "columns": ["time", "value"],
@@ -110,6 +233,7 @@ class DeviceGraph extends Component {
         combinedSeries: combinedSeries, 
         samples: series,
         error: false,
+        acceptableVariance: this.checkAcceptableVariance(),
       });
 
     }) 

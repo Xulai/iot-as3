dbf14b4c42e8214ab9c0e2ef5409fcf466d2d5a2
Dharius Robinson <dr296@kent.ac.uk>
2017-12-09 14:46:18 +0000

    Added completed data smoothing

--- src/Device/DeviceGraph.js
+++ src/Device/DeviceGraph.js
@@ -46,104 +46,160 @@ class DeviceGraph extends Component {
 
   isAnomalous(val1, val2)
   {
-	  var var1Variance = (val1/100) * 10;//this.checkAcceptableVariance();
+	  var var1Variance = (val1/100) * this.checkAcceptableVariance();
 	  if(Math.abs(val1 - val2) > var1Variance) {
 		 return true; 
 	  }
 	  return false;
   }
 
-  checkAndFixAnomalousVals(values) {
-    var prePreviousValue;
-	  var middleValue;
-	  var currentValue;
-    for(var i = 2; i < values.length; i++) {
-      prePreviousValue = values[i-2][1];
-      middleValue = values[i-1][1];
-      currentValue = values[i][1];
-      if(this.isAnomalous(prePreviousValue, middleValue) && this.isAnomalous(currentValue, middleValue)) { //this means that the middle value is out of range of both surrounding values
-        console.log([values[i-1][1], (prePreviousValue + currentValue) / 2]);
-        console.log([prePreviousValue, middleValue, currentValue]);
-        values[i-1][1] = (prePreviousValue + currentValue) / 2; //set anomalous middle value to average of surrounding values
-        console.log(values[i-1][1]);
-      }
-    }
-    return values;
-  }
+  // checkAndFixAnomalousVals(values) {
+  //   var prePreviousValue;
+	//   var middleValue;
+	//   var currentValue;
+  //   for(var i = 2; i < values.length; i++) {
+  //     prePreviousValue = values[i-2][1];
+  //     middleValue = values[i-1][1];
+  //     currentValue = values[i][1];
+  //     if(this.isAnomalous(prePreviousValue, middleValue) && this.isAnomalous(currentValue, middleValue)) { //this means that the middle value is out of range of both surrounding values
+  //       //console.log([values[i-1][1], (prePreviousValue + currentValue) / 2]);
+  //       //console.log([prePreviousValue, middleValue, currentValue]);
+  //       values[i-1][1] = (prePreviousValue + currentValue) / 2; //set anomalous middle value to average of surrounding values
+  //       //console.log(values[i-1][1]);
+  //     }
+  //   }
+  //   return values;
+  // }
 
-  findEndPosOfTimespan(values, startTime, timespan)
-  {
-    for(var position = 0; position < values.length; position++)
-    {
-      if((values[position][0] - timespan - timespan) > 0)
-      {
-        if (position === values.length - 1)
-          return values.length - 2;
-        else
-          return position;
-      }
-      return position;
-    }
-    return null;
-  }
+  // findEndPosOfTimespan(values, startPos, timespan)
+  // {
+  //   var endOfArray = values.length-1;
+  //   for(var position = startPos; position < endOfArray; position++)
+  //   {
+  //     if((values[position][0] - timespan) >= 0)
+  //     {
+  //       if (position >= endOfArray)
+  //         return endOfArray-1; //so that we don't try to compare the current (end) value to the non-existant next value;
+  //       else
+  //         return position;
+  //     }
+  //   }
+  //   return null;
+  // }
 
-  findPosition(values, startTime)
-  {
-    for(var position = 0; position < values.length; position++) 
-    {
-      if (values[position][0] > startTime)
-        if (position === 0)
-          return 1;
-        else
-          return position;
-    }
-    return null;
-  }
+  // findPosition(values, startTime)
+  // {
+  //   for(var position = 0; position < values.length; position++) 
+  //   {
+  //     if (values[position][0] >= startTime)
+  //       // if (position === 0)
+  //       //   return 1;
+  //       // else
+  //       return position;
+  //   }
+  //   return null;
+  // }
 
-  avgOfTimespan(values, timeSpan, startTime)
+  avgOfTimespan(values, timeSpan, startTime, pos)
   {
     var total = 0;
-    var counter = this.findPosition(values, startTime);
-    while ((values[counter][0] - startTime - timeSpan) <= 0) {
-      if(counter >= values.length)
-      {
-        return total/counter;
-      }
-      total += values[counter][1];
-      counter++;
+    var n = 0;
+    var currentTime = values[pos][0];
+    var endOfArray = values.length-1;
+    while ((currentTime - startTime - timeSpan) <= 0) {
+      total += values[pos][1];
+      pos++;
+      n++;
+      if(pos >= endOfArray)
+        return {
+          "avg": total/n,
+          "endPos": endOfArray-1 //so that we don't try to compare the current (end) value to the non-existant next value;
+        };
+      currentTime = values[pos][0];
     }
-    return total/counter;
+    return {
+      "avg": total/n,
+      "endPos": pos-1
+    };
   }
+
+  // calcAvg(values)
+  // {
+  //   var n = values.length;
+  //   var total = 0;
+  //   for(var i = 0; i < n; i++)
+  //   {
+  //     total += values[i];
+  //   }
+  //   return total/n;
+  // }
+
+  // movingAverage(values, sectionSize) {
+  //   var smoothedValues = values.slice(0, values.length - sectionSize-1);
+  //   //var currentAvgSection;
+  //   //var sectionAvg;
+  //   var sectionTotal;
+  //   var n;
+  //   for(var i = 0; i < values.length - sectionSize; i++)
+  //   {
+  //     n = 0;
+  //     sectionTotal = 0;
+  //     for(var pos = i; pos <= pos + sectionSize; pos++)
+  //     {
+  //       if(pos < values.length)
+  //       {
+  //         sectionTotal += values[pos][1];
+  //         n++;
+  //       }
+  //     }
+  //     smoothedValues[i][1] = sectionTotal/n;
+  //     smoothedValues[i][0] = values[i][0];
+  //   }
+  //   return smoothedValues;
+  // }
 
   smoothValues(values) {
     var timespan;
     var avgofTimespan;
-    var avg;
-    var startTime = values[0][1];
-    if(this.props.sampleRate = "minute")
+    //var avg;
+    var currentVal;
+    var preVal;
+    var nextVal;
+    var startPos = 0;
+    var startTime = values[0][0];
+    if(this.props.sampleRate === "minute")
       timespan = 1000; // 1 second
-    else if(this.props.sampleRate = "10minute")
-      timespan = 10000; // 10 seconds
-    else if(this.props.sampleRate = "hour")
+    else if(this.props.sampleRate === "10minute")
+      timespan = 5000; //10000; // 10 seconds
+    else if(this.props.sampleRate === "hour")
       timespan = 60000; // 1 minute
     
     while(startTime <= values[values.length-1][0]){
-      avgofTimespan = this.avgOfTimespan(values, timespan, startTime);
-      for(var i = this.findPosition(values, startTime); i <= this.findEndPosTimespan(values, startTime, timespan); i++)
+      avgofTimespan = this.avgOfTimespan(values, timespan, startTime, startPos);
+      //console.log(avgofTimespan.avg, values, timespan, startPos, avgofTimespan.endPos);
+      for(var i = startPos; i <= avgofTimespan.endPos; i++)
       {
         //if(isAnomalous(values[i-1][0], values[i][0]) && isAnomalous(values[i+1][0], values[i][0]))
-        avg = (values[i-1][0] + values[i+1][0]) / 2;
-        if(Math.abs(avgofTimespan - avg) > (avgofTimespan / 10))
+        if(i === 0)
+          i++;
+        preVal = values[i-1][1];
+        currentVal = values[i][1];
+        nextVal = values[i+1][1];
+        //avg = (preVal + nextVal) / 2;
+        if(this.isAnomalous(preVal, currentVal) || this.isAnomalous(nextVal, currentVal))//) > (avgofTimespan.avg / 10)) //difference is more than 10% of avgofTimespan
         {
-          if((avgofTimespan - avg) > 0)
-            values[i][0] = avg + (avgofTimespan/10);
+          //console.log(values[i][1]);
+          if((preVal - currentVal) > 0)
+          {
+            values[i][1] = preVal - (avgofTimespan.avg/10);
+          }
           else
-            values[i][0] = avg - (avgofTimespan/10);
+            values[i][1] = preVal + (avgofTimespan.avg/10);
         }
       }
+      startPos = avgofTimespan.endPos;
       startTime += timespan;
     }
-
     return values;
   }
 
@@ -157,8 +213,10 @@ class DeviceGraph extends Component {
 
       if(!_.isEmpty(response.data.light_value)) {
         sensorName = "light";
-        values = this.smoothValues(response.data.light_value.map(value => parseInt[moment(value[0]).format('X'), value[1]]));
-        //values = response.data.light_value.map(value => [parseInt(moment(value[0]).format('X')), value[1]]);
+        //console.log(response.data.light_value);
+        values = response.data.light_value.map(value => [parseInt(moment(value[0]).format('X')), value[1]]);
+        //values = this.movingAverage(values, 20);
+        values = this.smoothValues(values);
         //values = this.checkAndFixAnomalousVals(values);
         data = {
           "name": "Light values",
